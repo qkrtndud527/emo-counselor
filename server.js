ad9386b57a671825031b5c9bf1813b6ded2c2849
qkrtndud527 <qkrtndud123456@gmail.com>
2026-04-19 16:32:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
 const app = express();
 app.use(cors());
